98ead7962b3056a16e8353d49b74df8644c6df46
Gert Lužkov <geluzk@ttu.ee>
2020-02-15 18:52:30 +0200

    Alustasin kodutoo 17 veebruar, tegin esimesed 5

---
+++ Kodutoo17Veebruar/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Kodutoo17Veebruar.Models;
+using Microsoft.EntityFrameworkCore;
+using Kodutoo17Veebruar.Data;
+using Kodutoo17Veebruar.Models.SchoolViewModels;
+
+namespace Kodutoo17Veebruar.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SchoolContext _context;
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
