ff589f2f33c69e86a87bb445a23c38abb6573887
Gert Lužkov <geluzk@ttu.ee>
2020-02-19 09:55:59 +0200

    17 veebruar kodutoo exception parandus

--- Kodutoo17Veebruar/Controllers/InstructorsController.cs
+++ Kodutoo17Veebruar/Controllers/InstructorsController.cs
@@ -28,12 +28,9 @@ namespace Kodutoo17Veebruar.Controllers
                   .Include(i => i.OfficeAssignment)
                   .Include(i => i.CourseAssignments)
                     .ThenInclude(i => i.Course)
-                        .ThenInclude(i => i.Enrollments)
-                            .ThenInclude(i => i.Student)
                   .Include(i => i.CourseAssignments)
                     .ThenInclude(i => i.Course)
                         .ThenInclude(i => i.Department)
-                  .AsNoTracking()
                   .OrderBy(i => i.LastName)
                   .ToListAsync();
 
